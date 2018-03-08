d79d7661c36694dfe4cb47a1b3627ed582b60c8b
Daniel Brownridge <daniel@yobota.xyz>
2018-03-08 18:08:38 +0000

    IN-912 add API endpoint GET check

--- test.sh
+++ test.sh
@@ -1,7 +1,26 @@
 echo "heroku-buildpack-python-requirements"
+HEROKU_API_URL="https://api.heroku.com/"
+HEROKU_VERSION_HEADER="Accept: application/vnd.heroku+json; version=3"
 
 # check a Heroku API key is available
-if [ -z "$HEROKU_API_KEY" ]; then
+
+if [ -n "$HEROKU_API_KEY" ]; then
+    echo "Heroku API key found"
+else
     echo "Heroku API key not set"
+    exit 1
+fi
+
+
+# check can access a Heroku API endpoint
+
+APPS_URL="${HEROKU_API_URL}apps"
+GET_APPS_STATUS="curl -s -o /dev/null -w %{http_code} $APPS_URL"
+response=$($GET_APPS_STATUS)
+
+if [ $response -eq 200 ]; then
+    echo "GET $APPS_URL success"
+else
+    echo "GET $APPS_URL $response"
     exit 1
 fi
